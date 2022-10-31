38b3136b5118356d7d05eaa15557d1f3377e0ea4
domster704 <domster704@mail.ru>
2022-10-31 15:53:07 +0300

    add correcting params

--- api/cian_parser_api.py
+++ api/cian_parser_api.py
@@ -16,16 +16,26 @@ class CianParser(object):
         self.__cloudscraper.headers = {
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"
         }
-        self.__geocodeID = 0
-        self.__listOfFlatsID = []
-        self.__listOfFlatsLink = []
-        self.__listOfFlatParams = []
+        self.__geocodeID: int = 0
+        self.__listOfFlatParams: list = []
 
-        self.__address = "".join(dictWithData["address"].split(',')[:-1])
-        self.__rooms = dictWithData["room"]
-        self.__segment = str(dictWithData["segment"]).lower()
-        self.__maxFloor = dictWithData["maxFloor"]
-        self.__material = str(dictWithData["material"]).lower()
+        # ------------- Обязательные параметры ---------------
+        self.__address: str = "".join(dictWithData["address"].split(',')[:-1])
+        self.__rooms: int = dictWithData["room"]
+        self.__segment: str = str(dictWithData["segment"]).lower()
+        self.__maxFloor: int = dictWithData["maxFloor"]
+        self.__material: str = str(dictWithData["material"]).lower()
+
+        # ------------- Корректирующие параметры -------------
+        self.__flatFloor: int = dictWithData["correctFloor"]
+        self.__flatArea: str = dictWithData["correctArea"]
+        self.__flatKitchenFloor: str = dictWithData["correctKitchenArea"]
+        self.__flatBalcony: bool = True if str(dictWithData["correctBalcony"]).lower() == "true" else False
+        self.__metroTime: int = dictWithData["correctMetroTime"]
+        self.__flatSegment: str = self.__segment
+
+        self.__typeOfEvalFloor = CorrectParam.getTypeOfFloor(self.__flatFloor, self.__maxFloor)
+        self.__typeOfEvalArea = CorrectParam.getTypeOfFloor(self.__flatFloor, self.__maxFloor)
 
     def parse(self):
         self.__geoID(self.__address)
@@ -38,7 +48,7 @@ class CianParser(object):
         e.g. address = г. Москва, ул. Ватутина, д. 11
         """
 
-        def getCoord():
+        def getCoord() -> list:
             """
             :return: список из координат формата [x, y]
             """
@@ -129,11 +139,12 @@ class CianParser(object):
 
         for name in ["offersSerialized", "suggestOffersSerializedList"]:
             for elem in responseOfOffers.json()["data"][name]:
-                isThereBalcony = True if type(elem["balconiesCount"]) is int and elem["balconiesCount"] > 0 else False
-                metroTime = 100000
+                isThereBalcony: bool = True if type(elem["balconiesCount"]) is int and elem[
+                    "balconiesCount"] > 0 else False
+                metroTime: int = 100000
                 for metroData in elem["geo"]["undergrounds"]:
                     metroTime = min(metroData["time"], metroTime)
-
+                
                 data = {
                     # "id": elem["id"],
                     # "buildingData": elem["building"],
@@ -148,6 +159,10 @@ class CianParser(object):
                     "balcony": str(isThereBalcony),
                     "metroTime": metroTime,
                     "segment": self.__segment,
+                    "typeOfFloor": {
+                        "x": self.__typeOfEvalFloor,
+                        "y": CianParser.__getTypeOfFloor(elem["floorNumber"], elem["building"]["floorsCount"])
+                    },
                     "location": {
                         "lat": str(elem["geo"]["coordinates"]["lat"]),
                         "lng": str(elem["geo"]["coordinates"]["lng"]),
@@ -155,3 +170,90 @@ class CianParser(object):
                 }
 
                 self.__listOfFlatParams.append(data)
+
+
+class CorrectParam(object):
+    @staticmethod
+    def getTypeOfFloor(floor: int, maxFloor: int) -> int:
+        """
+        Определяет номер строки/столбца для корректировки из таблицы в "Приложении 6"
+        :param floor: этаж квартиры.
+        :param maxFloor: этажность дома.
+        :return:
+        Красных зон нет
+        """
+        status: int = 0
+        percent = int(floor / maxFloor) * 100
+        if 30 <= percent <= 70:
+            status = 1
+        elif percent > 70:
+            status = 2
+        return status
+
+    @staticmethod
+    def getTypeOfArea(area: float) -> int:
+        """
+        Определяет номер строки/столбца для корректировки из таблицы в "Приложении 6"
+        :param area: площадь квартиры.
+        :return: status - индекс
+        if status in [3, 4, 5], то это красная зона
+        """
+        status: int = 0
+        if 30 <= area <= 50:
+            status = 1
+        elif 50 < area <= 65:
+            status = 2
+        elif 65 < area <= 90:
+            status = 3
+        elif 90 < area <= 120:
+            status = 4
+        elif area > 120:
+            status = 5
+        return status
+
+    @staticmethod
+    def getTypeOfKitchenArea(kitchenArea: float) -> int:
+        """
+        Определяет номер строки/столбца для корректировки из таблицы в "Приложении 6"
+        :param kitchenArea: площадь кухни
+        :return: status - индекс
+        Красных зон нет
+        """
+        status: int = 0
+        if 7 <= kitchenArea <= 10:
+            status = 1
+        elif 10 < kitchenArea <= 15:
+            status = 2
+        return status
+
+    @staticmethod
+    def getTypeOfBalcony(isBalcony: bool) -> int:
+        """
+        Определяет номер строки/столбца для корректировки из таблицы в "Приложении 6"
+        :param isBalcony: наличие балкона в квартире
+        :return: status - индекс
+        Красных зон нет
+        """
+        status: int = 1 if isBalcony else 0
+        return status
+
+    @staticmethod
+    def getTypeOfMetroTime(metroTime: int) -> int:
+        """
+        Определяет номер строки/столбца для корректировки из таблицы в "Приложении 6"
+        :param metroTime: время в минутах до метро.
+        :return: status - индекс
+        if status in [3, 4, 5], то это красная зона
+        """
+        status: int = 0
+        if 5 <= metroTime <= 10:
+            status = 1
+        elif 10 < metroTime <= 15:
+            status = 2
+        elif 15 < metroTime <= 30:
+            status = 3
+        elif 30 < metroTime <= 60:
+            status = 4
+        elif 60 < metroTime <= 90:
+            status = 5
+        return status
